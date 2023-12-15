a0f57952e356cbfd03c0b669b4328c705eb8b9fe
Pedro Souza <pedroantoniidm@gmail.com>
2023-12-15 18:46:30 -0300

    Commit teste

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+        float numeroUm = 0.0f;
+        float numeroDois = 0.0f;
+
+        System.out.println("Digite um Número: ");
+        numeroUm = entrada.nextFloat();
+
+        System.out.println("Digite um Número: ");
+        numeroDois = entrada.nextFloat();
+
+        Operacoes.soma(numeroUm,numeroDois);
+        Operacoes.substracao(numeroUm,numeroDois);
+        Operacoes.multiplicacao(numeroUm,numeroDois);
+        Operacoes.divisao(numeroUm,numeroDois);
+        entrada.close();
+
+
+
+
     }
 }
---
+++ src/Operacoes.java
@@ -0,0 +1,14 @@
+public class Operacoes {
+    public static void soma( float x , float y) {
+        System.out.println(x + y);
+    }
+    public static void substracao( float x , float y) {
+        System.out.println(x - y);
+    }
+    public static void multiplicacao( float x , float y) {
+       System.out.println(x * y);
+    }
+    public static void divisao( float x , float y) {
+        System.out.println(x / y);
+    }
+}
