4e81d7d3e7c1c2c3134984927c589b1042ef7b8b
Pedro Souza <pedroantoniidm@gmail.com>
2023-12-15 17:59:50 -0300

    Commit test

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,4 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
-        // 1 2 3 4 5 6 7 8 9 10
-        // para uma variável que inicia em 1, e vai até 10, mudando 1 por 1, faça:
-
-        for (int i = 1; i <= 7; i++) {
-            for (int j = 1; j <= 10; j++) {
-                System.out.println(j + "x" + i + " = " + j * i);
-            }
-        }
     }
 }
